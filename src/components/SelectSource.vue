9c4e98e68ed20c4e84d231ac6aead0b2bdf4bb82
Antonio Molinari <molinari@incode.it>
2024-06-05 02:16:56 +0200

    video/audio recoding

---
+++ src/components/SelectSource.vue
@@ -0,0 +1,28 @@
+<script setup>
+//https://vuejs.org/guide/components/v-model
+
+import { ref, onMounted, defineProps } from "vue";
+
+const props = defineProps({
+  "sources": {
+    type: Array,
+    required: true,
+  },
+});
+const source = defineModel();
+
+</script>
+
+<template>
+  <select 
+    class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-200" 
+    v-model="source" 
+  >
+    <option v-for="source in sources" :key="source.deviceId" :value="source.deviceId">
+      {{ source.label }}
+    </option> 
+  </select>
+</template>
+
+<style scope lang="scss">
+</style>
--- src/components/VideoForm.vue
+++ src/components/VideoForm.vue
@@ -1,6 +1,8 @@
 <script setup>
-import { ref, onMounted, computed } from "vue";
+import { ref, onMounted, computed, defineModel } from "vue";
+import SelectSource from "./SelectSource.vue";
 
+const videoFormat = "webm";
 const recording = ref(false);
 const recordingDatetimeStart = ref(null);
 const recordingInfoInterval = ref(null);
@@ -8,34 +10,62 @@ const recordingInfo = ref(null);
 let recorder = null;
 let stream = null;
 
+const sourcesVideo = ref([]);
+const sourcesAudio = ref([]);
+const sourceVideo = defineModel("sourceVideo", {
+  type: String,
+  default: null,
+});
+const sourceAudio = defineModel("sourceAudio", {
+  type: String,
+  default: null,
+});
+
+const getSources = async () => {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  sourcesVideo.value = devices.filter((device) => device.kind === "videoinput");
+  sourcesAudio.value = devices.filter((device) => device.kind === "audioinput");
+  console.log("devices", devices);
+};
+
+
 const setRecordingInfo = () => {
   const duration = new Date() - recordingDatetimeStart.value;
   const date = new Date(duration);
   recordingInfo.value = date.toISOString().substr(11, 8);
 };
 
-const setVideo = async () => {
-  stream = await navigator.mediaDevices.getUserMedia({ video: true });
+
+const setupRecording = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    video: {
+      deviceId: sourceVideo.value,
+    },
+    audio: {
+      deviceId: sourceAudio.value,
+    },
+  });
   const videoDiv = document.getElementById("video");
   const video = document.createElement("video");
   videoDiv.innerHTML = "";
   videoDiv.appendChild(video);
   video.srcObject = stream;
+  video.volume = 0;
   await video.play();
 };
 
-// record the video from the webcam
+
 const startRecording = async () => {
   const chunks = [];
   recorder = new MediaRecorder(stream);   
   recorder.ondataavailable = (e) => chunks.push(e.data);
   recorder.onstop = (e) => {
-    const blob = new Blob(chunks, { type: "video/webm" });
+    const blob = new Blob(chunks, { type: `video/${videoFormat}` });
     const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
     document.body.appendChild(a);
     a.href = url;
-    a.download = "video.webm";
+    a.download = `video.${videoFormat}`;
     a.click();
     window.URL.revokeObjectURL(url);
   };
@@ -44,7 +74,6 @@ const startRecording = async () => {
   setRecordingInfo();
   recordingInfoInterval.value = setInterval(setRecordingInfo, 1000);
   recording.value = true;
-  //await new Promise((resolve) => setTimeout(resolve, 5000));
 };
 
 const stopRecording = () => {
@@ -54,7 +83,7 @@ const stopRecording = () => {
 };
 
 onMounted(async () => {
-  await setVideo();
+  await getSources();
 });
 </script>
 
@@ -62,16 +91,42 @@ onMounted(async () => {
   <div class="flex justify-center">
     <div class="w-1/2">
       <div class="p-4">
-        <h1 class="text-2xl font-bold">Video</h1>
-        <div id="video" class="mb-4"></div>
-        <button @click="() => {
-          recording ? stopRecording() : startRecording();
-        }" class="bg-blue-500 text-white px-4 me-2 py-2 rounded">
-          {{ recording ? "Stop recording" : "Record video" }}
-        </button>
-        <div v-if="recording" class="inline">
-          <span>{{ recordingInfo }}</span>
+        <h1 class="text-2xl font-bold mb-2">Record a video</h1>
+        <div id="sources" class="mb-2">
+          <div class="md:flex md:items-center mb-2">
+            <div class="md:w-1/4">
+              <label class="text-lg " for="source">Video</label>
+            </div>
+            <div class="md:w-3/4">
+              <SelectSource :sources="sourcesVideo" v-model="sourceVideo" />
+            </div>
+          </div>
+          <template v-if="sourceVideo">
+            <div class="md:flex md:items-center mb-2">
+              <div class="md:w-1/4">
+                <label class="text-lg " for="source">Audio</label>
+              </div>
+              <div class="md:w-3/4">
+                <SelectSource 
+                  class="mb-1 inline-block" 
+                  :sources="sourcesAudio" 
+                  v-model="sourceAudio"
+                  @update:modelValue="setupRecording" 
+                />
+              </div>
+            </div>
+          </template>
         </div>
+
+        <div id="video" class="mb-2"></div>
+        <template v-if="sourceVideo && sourceAudio">
+          <button @click="() => {recording ? stopRecording() : startRecording()}" class="bg-blue-500 text-white px-4 me-2 py-2 rounded w-full">
+            {{ recording ? "Stop recording" : "Record video" }}
+          </button>
+          <div v-if="recording" class="inline">
+            <span>{{ recordingInfo }}</span>
+          </div>
+        </template>
       </div>
     </div>
   </div>
